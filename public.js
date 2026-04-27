9efe3344ef68ee3078ce47dd2ee610240abe661b
geh-lab <envlab1315@gmail.com>
2026-04-27 22:06:25 +0900

    Add files via upload

--- public.js
+++ public.js
@@ -1,4 +1,4 @@
-import { BUILD_DATE, SITE_COPY, FALLBACK_MEMBERS, FALLBACK_PROJECTS, FALLBACK_PUBLICATIONS, FALLBACK_BOARD_POSTS } from './data.js?v=67';
+import { BUILD_DATE, SITE_COPY, FALLBACK_MEMBERS, FALLBACK_PROJECTS, FALLBACK_PUBLICATIONS, FALLBACK_BOARD_POSTS } from './data.js?v=68';
 import {
   escapeHTML,
   getInitials,
@@ -24,8 +24,8 @@ import {
   normalizeProjectPeriod,
   isActiveItem,
   formatEnglishName
-} from './utils.js?v=67';
-import { hasFirebaseConfig, fetchCollection, listenCollection, COLLECTIONS } from './firebase.js?v=67';
+} from './utils.js?v=68';
+import { hasFirebaseConfig, fetchCollection, listenCollection, COLLECTIONS } from './firebase.js?v=68';
 
 const body = document.body;
 const page = body.dataset.page;
@@ -74,7 +74,7 @@ const modalState = {
   closeButtons: []
 };
 
-const PUBLIC_CACHE_KEY = 'geh-public-cache-v67';
+const PUBLIC_CACHE_KEY = 'geh-public-cache-v68';
 
 
 function cacheFresh(cache = {}, minutes = 15) {
