c87eda18cbed86abbc2a73b7dd9f43ecd3636306
geh-lab <envlab1315@gmail.com>
2026-07-14 20:22:06 +0900

    Add files via upload

---
+++ dist/firebase-config.js
@@ -0,0 +1,13 @@
+window.GEH_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAnUC-UYmLHjdCWARGUHXfgOeVUm1gjUOs",
+  authDomain: "cnu-geh-lab.firebaseapp.com",
+  databaseURL: "https://cnu-geh-lab-default-rtdb.firebaseio.com",
+  projectId: "cnu-geh-lab",
+  storageBucket: "cnu-geh-lab.firebasestorage.app",
+  messagingSenderId: "841207426499",
+  appId: "1:841207426499:web:2165fa5e2e93a0c70de176"
+};
+
+window.GEH_ADMIN_EMAILS = [
+  "envlab1315@gmail.com"
+];
